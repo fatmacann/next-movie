f7cd065b32fec6abc5362754cee32ebb81829dd4
fatmacann <canf261012@gmail.com>
2024-05-11 16:51:57 +0300

    sidebar

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+import Sidebar from './components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-
+        <Sidebar />
         {children}
       </body>
     </html>
