c9790286ed1b8eeaa834de13edc47537e9a88bfb
fatmacann <canf261012@gmail.com>
2024-05-11 15:47:08 +0300

    movie explorer

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const Layout = ({children}) => {
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Movie Explorer',
+  description: 'Your one stop shop for seeing upcoming, now playing, top rated, and all movie related stuff.',
+}
+
+export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-        <body>{children}</body>
+    <html lang="en">
+      <body className={inter.className}>
 
+        {children}
+      </body>
     </html>
   )
 }
-
-export default Layout
--- app/products/[[...id]]/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Page = ({params}) => {
-  return (
-    <div>page detailll {params.id}</div>
-  )
-}
-
-export default Page
